bc121f78edb5d034b1636ea5003270b26f86bdf6
Jordy Devrix <2109jordy2241@gmail.com>
2024-09-06 14:57:49 +0200

    Added string concatenation header and imported it into main.c

--- main.c
+++ main.c
@@ -3,11 +3,20 @@
 
 #include "firstheader.h"
 #include "mallocchecker.h"
+#include "stringConcatenation.h"
 
 int main() {
+    // Check if malloc works
     checkMalloc();
-    
+
+    // Check if I can add 10 to a value
     int val = addTenToVal(5);
     printf("Value: %d\n", val);
+
+    // Check if I can concatenate strings
+    char *myConcatenatedString = concatenateStrings("Hello", " World");
+    printf("Concatenated string: %s\n", myConcatenatedString);
+
+    return 0;
 }
 
---
+++ stringConcatenation.h
@@ -0,0 +1,11 @@
+#ifndef STRING_CONCATENATION_H
+#define STRING_CONCATENATION_H
+
+#include<stdio.h>
+
+char *concatenateStrings(char *strOne, char *strTwo) {
+    char *strFinal;
+    return strFinal;
+}
+
+#endif
