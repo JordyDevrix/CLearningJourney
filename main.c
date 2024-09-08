bd799c991478bd2da9c1c0c4ef309bfd294d3d8d
Jordy Devrix <2109jordy2241@gmail.com>
2024-09-08 17:40:54 +0200

    working on arrays with strings

--- main.c
+++ main.c
@@ -25,6 +25,28 @@ int main() {
     printf("Concatenated string after free'ing: %s\n", myConcatenatedString);
     myConcatenatedString = NULL;
     printf("Concatenated string after assigning NULL value: %s\n", myConcatenatedString);
+    
+    char *wordOne = "Hello";
+    char *wordTwo = " World";
+
+    char *arr[] = {wordOne, wordTwo};
+
+    arr[0] = wordOne;
+    arr[1] = wordTwo;
+
+    printf("elements: %d\n", sizeof(arr[0]));
+
+    int *sizeOfArray = malloc(sizeof(int));
+    *sizeOfArray = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < *sizeOfArray; i++) 
+    {
+        printf("%s\n", arr[i]);
+    }
+    
+    printf("Size of array before free'ing: %d\n", *sizeOfArray);
+    free(sizeOfArray);
+    printf("Size of array after free'ing: %d\n", *sizeOfArray);
 
     return 0;
 }
--- strconc.h
+++ strconc.h
@@ -3,7 +3,14 @@
 
 char *concatenateStrings(char strOne[], char strTwo[]) {
     int requiredLength = strlen(strOne) + strlen(strTwo);
+
     char *strFinal = malloc(sizeof(char) * (requiredLength + 1));
+    
+    int *i = malloc(sizeof(int));
+    *i = 33;
+    printf("Value of i: %d\n", *i);
+    free(i);
+    printf("Value of i after free'ing: %d\n", *i);
 
     for (int i = 0; i < strlen(strOne); i++) {
         strFinal[i] = strOne[i];
